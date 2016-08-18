336ccd4981c8bdfae64c119647a0504aa81ce0e1
Neil <vitalene@msu.edu>
2016-08-18 15:33:12 -0400

    Initial Commit

---
+++ week2RPG/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  week2RPG
+//
+//  Created by Neil Vitale on 8/18/16.
+//  Copyright © 2016 Neil Vitale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
